fd498d482f69365c13e6cc2d1ed2f945f0b0fdfa
ericwm76 <eric.meldrum@gmail.com>
2019-12-18 18:54:21 -0700

    Create NavBar component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import NavBar from '../NavBar/NavBar'
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './custom.scss';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
