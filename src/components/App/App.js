955c5e2a29980ce242a3aacf0021b4e079b86700
ericwm76 <eric.meldrum@gmail.com>
2020-01-05 18:24:53 -0700

    Create useState hook in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import NavBar from '../NavBar/NavBar';
 import CardContainer from '../CardContainer/CardContainer'
+import data from '../../data/seedData'
 
 const App = () => {
+  const [courses, setCourses] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const getCourses = async () => {
+    setError('')
+    setIsLoading(true)
+    try {
+      setCourses(data)
+    } catch(error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getCourses()
+  }, [])
+
   return (
     <div className="App">
       <NavBar />
-      <CardContainer />
+      <CardContainer courses={courses}/>
     </div>
   );
 }
