a98682a610fba8978c3a59eadcf69aec2bd30280
ericwm76 <eric.meldrum@gmail.com>
2020-01-05 18:26:44 -0700

    Make CardsContainer component render as many cards as there are courses

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,10 +2,21 @@ import React from 'react';
 import CourseCard from '../CourseCard/CourseCard';
 import './CardContainer.scss';
 
-const CardContainer = () => {
+const CardContainer = ({ courses }) => {
+  const courseCards = courses.map(course => {
+    return (
+      <CourseCard 
+        id={course.id}
+        category={course.courseCategory}
+        title={course.snippet.title}
+        description={course.snippet.description}
+      />
+    )
+  })
+
   return (
     <div className="card-container">
-      <CourseCard />
+      {courseCards}
     </div>
   )
 }
