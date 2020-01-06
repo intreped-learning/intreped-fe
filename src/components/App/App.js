ad1daaedce7aa3281d4f3403bb427cebc9c4bc34
ericwm76 <eric.meldrum@gmail.com>
2020-01-06 13:28:04 -0700

    Import Route and add routes to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom'
 import './App.scss';
 import NavBar from '../NavBar/NavBar';
 import CardContainer from '../CardContainer/CardContainer'
@@ -27,7 +28,8 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <CardContainer courses={courses}/>
+      <Route exact path="/" component={CardContainer} courses={courses} />
+      <Route exact path="/courses/:id" component={CourseDetail} />
     </div>
   );
 }
