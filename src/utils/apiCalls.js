35cf3dcac6aed55b584189cd7711c2581f186f25
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-06 18:59:22 -0700

    Add add teacher course function to api calls

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -8,3 +8,27 @@ export const getCourses = async () => {
   const courses = await res.json();
   return courses;
 }
+
+export const addToTeacherCourses = async (courseId) => {
+  const body = {
+    teacher_id: 1,
+    "course_id": `${courseId}`,
+    current_time_marker: "0s",
+    is_favorite: true,
+    is_complete: false,
+    is_in_progress: false
+  }
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+  const res = await fetch(`${baseUrl}teacher_courses/`, options);
+  if (!res.ok) {
+    throw Error('Unable to add course to your list');
+  };
+  const addedCourse = await res.json();
+  return addedCourse;
+}
