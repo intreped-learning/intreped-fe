e9e45b618b2482b3fc31b977ce87a27f5683b7de
dawnlunacy <rudd.lacy@gmail.com>
2020-01-07 16:55:28 -0700

    Add error handling to card container when search result is not found

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import CourseCard from '../CourseCard/CourseCard';
 import './CardContainer.scss';
 
 const CardContainer = () => {
+  const dispatch = useDispatch();
   const { courses, currentCategory, currentSearch } = useSelector(state => state);
   let determineCourses = () => {
     if (currentCategory === "All Categories" ) {
@@ -14,7 +15,16 @@ const CardContainer = () => {
     }
   }
   let searchCourses = () => {
-    return determineCourses().filter(course => course.title.toLowerCase().includes(currentSearch.toLowerCase()) || course.description.toLowerCase().includes(currentSearch.toLowerCase()));
+    let filteredCourses = determineCourses().filter(course => course.title.toLowerCase().includes(currentSearch.toLowerCase()) || course.description.toLowerCase().includes(currentSearch.toLowerCase()));
+    if (filteredCourses.length === 0 ) {
+        dispatch({
+          type: 'CREATE_ERROR_MESSAGE',
+          payload: ` Could not find any videos related to your search of ${currentSearch}`
+        })
+        return []
+    } else {
+      return filteredCourses;
+    }
   }
 
 
