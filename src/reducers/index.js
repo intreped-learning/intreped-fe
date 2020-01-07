2ab4a7342c2d0f9d2da7ca87be486665f76519bd
dawnlunacy <rudd.lacy@gmail.com>
2020-01-07 14:04:21 -0700

    Add current Search reducer to the root redicer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { teacher } from './teacher';
 import { courses } from './courses';
 import { modalOpen } from './modalOpen';
 import { currentCategory } from './currentCategory'; 
+import { currentSearch } from './currentSearch';
 
 export const rootReducer = combineReducers({
   teacher,
   courses,
   modalOpen,
-  currentCategory
+  currentCategory,
+  currentSearch
 });
