2f281be421f9ecd85ad533dfc59c5f28f6063f1d
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-07 16:03:20 -0700

    Add courses in progress logic to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './Dashboard.scss';
+import CourseCard from '../CourseCard/CourseCard';
 import { useSelector } from 'react-redux';
 
 
 const Dashboard = () => {
   const { teacher } = useSelector(state => state);
+  const filterInProgress = teacher.teacher_courses.filter(course => course.is_in_progress === true);
+  const coursesInProgress = filterInProgress.map(course => {
+    return (
+      <CourseCard
+        key={course.id}
+        id={course.id}
+        category={course.category}
+        title={course.title}
+        description={course.description}
+        thumbnail = {course.thumbnail}
+      />
+    )
+  })
 
   return (
     <main className='dashboard'>
       <section className='courses-in-progress'>
-        
+        {coursesInProgress}
       </section>
       <section className='watch-list'>
 
