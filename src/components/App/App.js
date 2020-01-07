318fa7ba70ab7957e5880b241b4c0a2f77a0dc26
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-06 17:56:35 -0700

    Edit use effect to incorporate get courses api call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,35 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom'
 import './App.scss';
 import NavBar from '../NavBar/NavBar';
-import CardContainer from '../CardContainer/CardContainer'
-import CourseDetail from '../CourseDetail/CourseDetail'
-import data from '../../data/seedData'
+import CardContainer from '../CardContainer/CardContainer';
+import { getCourses } from '../../utils/apiCalls';
 
 const App = () => {
-  const [courses, setCourses] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  const getCourses = async () => {
-    setError('')
-    setIsLoading(true)
-    try {
-      setCourses(data)
-    } catch(error) {
-      setError(error.message)
-    }
-    setIsLoading(false)
-  }
+  const [courses, setCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
+    setError('')
+    setIsLoading(true)
     getCourses()
-  }, [])
+      .then(courses => setCourses(courses))
+      .catch(error => setError(error))
+    setIsLoading(false)
+  })
 
   return (
     <div className="App">
       <NavBar />
+<<<<<<< HEAD
       <Route exact path="/" render={() => <CardContainer courses={courses} /> } />
       <Route exact path="/courses/:id" render={({ match }) => <CourseDetail id={match.params.id} courses={courses} /> } />
+=======
+      <CardContainer courses={courses} />
+      {error && <h1>{error}</h1>}
+>>>>>>> Edit use effect to incorporate get courses api call
     </div>
   );
 }
