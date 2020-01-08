3fa9917eb4e0baaae45a784f85af7e504b59d65a
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-08 13:38:30 -0700

    Add conditional rendering for when a user is signed-in/out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Header = () => {
   const dispatch = useDispatch();
+  const { teacher } = useSelector(state => state)
 
+  const handleLogOut = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'LOGOUT'
+    })
+  }
   return (
     <header>
       <Link className="logo" id="logo" to='/'>
-        <h1> 
+        <h1>
           INTREP<span className="ed">ED</span>
         </h1>
       </Link>
-      <div>
-
-        <button className="sign-in-btn" onClick={() => dispatch({type:'TOGGLE_MODAL'})}> Sign In </button>
-        <button className="sign-up-btn"> Sign Up </button>
-      </div>
+      {!teacher.id &&
+        <div>
+          <button className="sign-in-btn" onClick={() => dispatch({ type: 'TOGGLE_MODAL' })}> Sign In </button>
+          <button className="sign-up-btn"> Sign Up </button>
+        </div>}
+      {teacher.id &&
+        <div className='signed-in-info'>
+        <p className='teacher-name'>Welcome {teacher.name}!</p>
+        <Link to='/dashboard' className='link-to-dashboard'>Dashboard</Link>
+        <button className='sign-out-btn' onClick={handleLogOut}>Sign Out</button>
+        </div>}
     </header>
   )
 }
