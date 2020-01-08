94b8e602c634ff6f2382b287de71206a389d089c
ericwm76 <eric.meldrum@gmail.com>
2020-01-07 17:04:10 -0700

    Write snapshot test for SignIn Modal

--- src/components/SignInModal/SignInModal.test.js
+++ src/components/SignInModal/SignInModal.test.js
@@ -1,5 +1,23 @@
-describe("magikCardContainer", () => {
-  it('should pass a test', () => {
-    expect(true).toEqual(true);
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import SignInModal from './SignInModal';
+import { rootReducer } from '../../reducers';
+
+describe('SignInModal', () => {
+  it('should match snapshot', () => {
+    const mockStore = createStore(rootReducer, {
+      teacher: 'Mr. Feeny',
+      courses: {},
+      modalOpen: true
+    })
+    const getWrapper = () => mount(
+      <Provider store={mockStore}>
+        <SignInModal/>
+      </Provider>
+    );
+    const wrapper = getWrapper();
+    expect(wrapper).toMatchSnapshot();
   })
-})
+});
