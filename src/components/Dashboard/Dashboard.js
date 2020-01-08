db898bd51426d3b5f4490f24ee85ef74072f6d62
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-07 17:18:53 -0700

    Add course filters to determine course status

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,35 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Dashboard.scss';
 import CourseCard from '../CourseCard/CourseCard';
 import { useSelector } from 'react-redux';
 
 
 const Dashboard = () => {
-  const { teacher } = useSelector(state => state);
+  const { courses, teacher } = useSelector(state => state);
+  
   const filterInProgress = teacher.teacher_courses.filter(course => course.is_in_progress === true);
-  const coursesInProgress = filterInProgress.map(course => {
-    return (
-      <CourseCard
-        key={course.id}
-        id={course.id}
-        category={course.category}
-        title={course.title}
-        description={course.description}
-        thumbnail = {course.thumbnail}
-      />
-    )
-  })
+
+  const filterWatchList = teacher.teacher_courses.filter(course => course.is_complete === false);
+
+  const getCourseInfo = (list) => {
+    return list.map(course => courses.filter( e => e.id === course.course_id)[0]);
+  }
+
+  const formatCourseInfo = list => {
+    return list.map(course => {
+    const route = `courses/${course.id}`
+      return (
+        <Link to={route}>
+          <div className='dashboard-course'>
+            <h3 className='course-title'>{course.title}</h3>
+            <img src={course.thumbnail} alt={course.title}/>
+          </div>
+        </Link>  
+      )
+    })
+  }
+
+  const watchlistCourses = getCourseInfo(filterWatchList)
+  const inProgressCourses = getCourseInfo(filterInProgress)
 
   return (
     <main className='dashboard'>
+      {console.log("watchList", watchlistCourses)}
+      {console.log('inProgess', inProgressCourses)
+      }
       <section className='courses-in-progress'>
-        {coursesInProgress}
+        <h2>In Progress</h2>
+        {formatCourseInfo(inProgressCourses)}
       </section>
       <section className='watch-list'>
-
+        <h2>Watch List</h2>
+        {formatCourseInfo(watchlistCourses)}
       </section>
       <section className='badges'>
-
+        <h2>Badges</h2>
       </section>
     </main>
   )
