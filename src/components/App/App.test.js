9cdb5ab04e9806fb29ee53d6768c74184e3f7a72
ericwm76 <eric.meldrum@gmail.com>
2020-01-08 15:52:15 -0700

    Change snapshot test in App component to use shallow() instead of mount()

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,24 +8,25 @@ import App from './App';
 
 const mockStore = configureMockStore([thunk]);
 
-// If I use mount without <Router>, I get "You should not use <Link> outside a <Router>". 
-// If I use mount with <Router>, it tries to render all the child components.
-// If I use shallow, it says I need to wrap it in a provider.
-// If I use shallow and wrap App in a provider, it takes a snapshot of the provider, not of App. 
-
 describe('App', () => {
-  it.skip('should match snapshot', () => {
+  it('should match snapshot', () => {
     const store = mockStore({
       teacher: 'Mr. Feeny',
       courses: {},
       modalOpen: true
     });
-    const getWrapper = () => mount(
-        <Provider store={store}>
+    const getWrapper = () => shallow(
+        <Provider>
           <App/>
         </Provider>
     );
     const wrapper = getWrapper();
+    console.log(wrapper)
     expect(wrapper).toMatchSnapshot();
   });
 });
+
+// If I use mount without <Router>, I get "You should not use <Link> outside a <Router>". 
+// If I use mount with <Router>, it tries to render all the child components.
+// If I use shallow, it says I need to wrap it in a provider.
+// If I use shallow and wrap App in a provider, it takes a snapshot of the provider, not of App. 
