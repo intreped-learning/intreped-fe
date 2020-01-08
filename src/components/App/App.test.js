62bb9079184f9f2222dad9ba40d5c0a0e79f2851
ericwm76 <eric.meldrum@gmail.com>
2020-01-07 20:01:39 -0700

    Write snapshot test for App component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,15 +1,31 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router} from 'react-router-dom';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+const mockStore = configureMockStore([thunk]);
 
-describe("magikApp", () => {
-  it('should pass a test', () => {
-    expect(true).toEqual(true);
-  })
-})
+// If I use mount without <Router>, I get "You should not use <Link> outside a <Router>". 
+// If I use mount with <Router>, it tries to render all the child components.
+// If I use shallow, it says I need to wrap it in a provider.
+// If I use shallow and wrap App in a provider, it takes a snapshot of the provider, not of App. 
+
+describe('App', () => {
+  it.skip('should match snapshot', () => {
+    const store = mockStore({
+      teacher: 'Mr. Feeny',
+      courses: {},
+      modalOpen: true
+    });
+    const getWrapper = () => mount(
+        <Provider store={store}>
+          <App/>
+        </Provider>
+    );
+    const wrapper = getWrapper();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
