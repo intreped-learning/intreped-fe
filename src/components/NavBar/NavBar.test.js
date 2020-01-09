08f877468b7bbcf1e3da90107d395b9c20cee9a2
ericwm76 <eric.meldrum@gmail.com>
2020-01-08 20:46:17 -0700

    Update NavBar snapshot test

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router} from 'react-router-dom';
 import NavBar from './NavBar';
 
 describe('NavBar', () => {
   it('should match snapshot', () => {
-    const getWrapper = () => shallow(
-      <NavBar/>
+    const mockStore = configureMockStore([thunk]);
+    const store = mockStore({
+      teacher: {
+        username: '',
+        id: null
+      },
+      courses: {},
+      modalOpen: true
+    });
+    const getWrapper = () => mount(
+      <Router>
+        <Provider store={store}>
+          <NavBar/>
+        </Provider>
+      </Router>
     );
     const wrapper = getWrapper();
     expect(wrapper).toMatchSnapshot();
