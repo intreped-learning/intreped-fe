e2230096e147c8f2b665c92ce4a169b40fd14014
ericwm76 <eric.meldrum@gmail.com>
2020-01-08 20:32:35 -0700

    Update App component snapshot test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk';
@@ -7,26 +7,25 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 
 const mockStore = configureMockStore([thunk]);
+const store = mockStore({
+  teacher: 'Mr. Feeny',
+  courses: [],
+  modalOpen: true
+});
+const getCourses = jest.fn();
+const useEffect = jest.fn().mockImplementation(() => getCourses())
+const getWrapper = () => mount(
+  <Router>
+    <Provider store={store}>
+      <App useEffect={useEffect}/>
+    </Provider>
+  </Router>
+);
+const wrapper = getWrapper();
 
 describe('App', () => {
   it('should match snapshot', () => {
-    const store = mockStore({
-      teacher: 'Mr. Feeny',
-      courses: {},
-      modalOpen: true
-    });
-    const getWrapper = () => shallow(
-        <Provider>
-          <App/>
-        </Provider>
-    );
-    const wrapper = getWrapper();
-    console.log(wrapper)
     expect(wrapper).toMatchSnapshot();
   });
 });
 
-// If I use mount without <Router>, I get "You should not use <Link> outside a <Router>". 
-// If I use mount with <Router>, it tries to render all the child components.
-// If I use shallow, it says I need to wrap it in a provider.
-// If I use shallow and wrap App in a provider, it takes a snapshot of the provider, not of App. 
