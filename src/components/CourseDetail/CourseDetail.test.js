1fa51076f361ec709e86dc940b309562e0a03a06
ericwm76 <eric.meldrum@gmail.com>
2020-01-08 21:27:41 -0700

    Update CourseDetail snapshot test

--- src/components/CourseDetail/CourseDetail.test.js
+++ src/components/CourseDetail/CourseDetail.test.js
@@ -7,22 +7,76 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import CourseDetail from './CourseDetail';
 
 const mockStore = configureMockStore([thunk]);
+const store = mockStore({
+  teacher: {
+    username: '',
+    id: null,
+    teacher_courses: [
+      {
+        "model": "intreped.course",
+        "id": 1,
+        "fields": {
+          "title": "Kagan Cooperative Learning: Inside-Outside Circle & Talking Chips",
+          "description": "Kagan structures are techniques and activities teachers can use in their classrooms to keep students engaged, active, and have fun while they are learning",
+          "url": "https://www.youtube.com/watch?v=B-hTXzUTxf8",
+          "thumbnail": "https://i.ytimg.com/vi/hTXzUTxf8/mqdefault.jpg",
+          "category": "Engagement Strategies",
+          "duration": "11m30s"
+        }
+      },
+      {
+        "model": "intreped.course",
+        "id": 2,
+        "fields": {
+          "title": "Think Pair Share Explained",
+          "description": "This video provides a succinct explanation for students about how to participate in THINK PAIR SHARE activities.",
+          "url": "https://www.youtube.com/watch?v=wW87rihT38I",
+          "thumbnail": "https://i.ytimg.com/vi/wW87rihT38I/mqdefault.jpg",
+          "category": "Engagement Strategies",
+          "duration": "1m35s"
+        }
+      }
+    ]
+  },
+  courses: [
+    {
+      "model": "intreped.course",
+      "id": 1,
+      "fields": {
+        "title": "Kagan Cooperative Learning: Inside-Outside Circle & Talking Chips",
+        "description": "Kagan structures are techniques and activities teachers can use in their classrooms to keep students engaged, active, and have fun while they are learning",
+        "url": "https://www.youtube.com/watch?v=B-hTXzUTxf8",
+        "thumbnail": "https://i.ytimg.com/vi/hTXzUTxf8/mqdefault.jpg",
+        "category": "Engagement Strategies",
+        "duration": "11m30s"
+      }
+    },
+    {
+      "model": "intreped.course",
+      "id": 2,
+      "fields": {
+        "title": "Think Pair Share Explained",
+        "description": "This video provides a succinct explanation for students about how to participate in THINK PAIR SHARE activities.",
+        "url": "https://www.youtube.com/watch?v=wW87rihT38I",
+        "thumbnail": "https://i.ytimg.com/vi/wW87rihT38I/mqdefault.jpg",
+        "category": "Engagement Strategies",
+        "duration": "1m35s"
+      }
+    }
+  ],
+  modalOpen: true
+});
+const getWrapper = () => mount(
+  <Router>
+    <Provider store={store}>
+      <CourseDetail id={1}/>
+    </Provider>
+  </Router>
+);
+const wrapper = getWrapper();
 
 describe('CourseDetail', () => {
   it('should match snapshot', () => {
-    const store = mockStore({
-      teacher: 'Mr. Feeny',
-      courses: {
-        course1: 'thing1'
-      },
-      modalOpen: true
-    });
-    const getWrapper = () => shallow(
-      <Provider store={store}>
-        <CourseDetail/>
-      </Provider>
-    );
-    const wrapper = getWrapper();
     expect(wrapper).toMatchSnapshot();
   });
 });
